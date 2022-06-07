32d8fdbf911886283e1d507a53ba9e3718689fe1
Marjolijn vanderRee <mvdree15@hotmail.com>
2022-06-07 17:44:48 +0100

    ranking is now descending

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -1,6 +1,6 @@
 class ClassroomsController < ApplicationController
   def show
     @students = Student.all
-    @sorted_students = @students.sort_by { |student| student[:coins] }
+    @sorted_students = @students.sort_by { |student| student[:coins] }.reverse!
   end
 end
