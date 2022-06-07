35ce974573b45f3a73a29a84de04882a5c1cacfb
Marlon Wanderllich <marlonwlv@gmail.com>
2022-06-07 12:21:06 -0300

    create edit index methods

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'classrooms#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :missions, only: [:edit, :update, :destroy]
+
   resources :classroom, only: [ :show ] do
     resources :students, only: %i[new create index]
     resources :missions, only: %i[new create index]
