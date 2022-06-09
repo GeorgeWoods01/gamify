8bbae419c0e5a1a5a71640f7cbc9ea7e773111de
Marlon Wanderllich <marlonwlv@gmail.com>
2022-06-09 07:17:57 -0300

    mission setup

--- app/controllers/mission_setups_controller.rb
+++ app/controllers/mission_setups_controller.rb
@@ -1,2 +1,17 @@
 class MissionSetupsController < ApplicationController
+  def new
+
+  end
+
+  def create
+    current_user.students.each do |student|
+      @mission_setup = MissionSetup.new(student: student)
+      @mission_setup.mission = Mission.find(params[:mission_id])
+      @mission_setup.save
+    end
+
+    raise
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'classrooms#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :missions, only: [:edit, :update, :destroy]
+  resources :missions, only: [:edit, :update, :destroy] do
+    resources :mission_setups, only: [:create]
+  end
 
   resources :classroom, only: [ :show ] do
     resources :students, only: %i[new create index]
