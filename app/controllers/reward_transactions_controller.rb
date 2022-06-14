ee8d3c1e01f03c09fc465693932b57361391fe9e
ricardo milhomens <ricardo.m.mil@hotmail.com>
2022-06-14 16:25:01 +0100

    fix rewards transactions

--- app/controllers/reward_transactions_controller.rb
+++ app/controllers/reward_transactions_controller.rb
@@ -11,11 +11,17 @@ class RewardTransactionsController < ApplicationController
   def create
     @reward = Reward.find(params[:reward_id])
     @student = Student.find(params[:reward_transaction][:student_id])
-    @reward_transaction = RewardTransaction.new(student: Student.find(params[:reward_transaction][:student_id]))
-    @reward_transaction.reward = @reward
-    @student.coins -= @reward.price
-    @student.save
-    @reward_transaction.save
-    redirect_to root_path
+    if @student.coins > @reward.price
+      @reward_transaction = RewardTransaction.new(student: Student.find(params[:reward_transaction][:student_id]))
+      @reward_transaction.reward = @reward
+      @student.coins -= @reward.price
+      @student.save
+      @reward_transaction.save
+      redirect_to root_path
+    else
+      @reward_transaction = RewardTransaction.new
+      render "new"
+      flash[:alert] = 'You cant buy this!'
+    end
   end
 end
